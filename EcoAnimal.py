eb682020edd43f3d7f5313269c83efed3d3c2fcd
akira <akiracraftwork@infoseek.jp>
2021-03-01 20:54:40 +0900

    EcoAnimalにGUIを統合

--- EcoAnimal.py
+++ EcoAnimal.py
@@ -1,8 +1,8 @@
 import wx
-from Gui import Gui
-from Animal import AnimalModel
+from Animal import AnimalModel, AnimalParameterSettingDialog, AnimalView
 from Market import Market
 import pandas as pd
+from SimpleBookSample import MainFrame
 
 
 # https://note.nkmk.me/python-pandas-assign-append/
@@ -26,7 +26,7 @@ class EcoAnimal:
             df['object'] = animal
             self.animal_list = self.animal_list.append(df, ignore_index=True)
 
-        self.view = Gui(None, self)
+        self.view = EcoAnimalView(None, self)
         self.view.Show()
 
     def get_animal_list(self):
@@ -42,6 +42,92 @@ class EcoAnimal:
         market = Market(self.get_animal_list(), self.view.logout)
         market.trade()
         self.view.update_animals()
+
+
+
+
+class EcoAnimalView(MainFrame):
+    def __init__(self, parent, model):
+        super().__init__(parent)
+
+        self.model = model
+        animal_model_list = self.model.get_animal_list()
+        self.bSizer_animal_list = wx.BoxSizer(wx.VERTICAL)
+
+        self.animal_list = []
+        for i in range(108):
+            animal = AnimalView(self.m_scrolledWindow, animal_model_list[i], wx.ID_ANY, wx.DefaultPosition,
+                                wx.DefaultSize, 0)
+            self.bSizer_animal_list.Add(animal, 1, wx.EXPAND | wx.ALL, 5)
+            self.animal_list.append(animal)
+            animal.set_root_window(self)
+
+        self.m_scrolledWindow.SetSizer(self.bSizer_animal_list)
+
+    def logout(self, text):
+        self.m_textCtrl.AppendText(text + '\n')
+
+    def onPaint(self, event):
+        for i in range(9):
+            for j in range(12):
+                x = j * 70 + 30
+                y = i * 40 + 20
+                animal = self.animal_list[i * 12 + j]
+                animal.paint(self.m_panel, x, y)
+
+    def onExit(self, event):
+        self.Destroy()
+
+    def onNext(self, event):
+        self.next_page()
+
+    def onPrev(self, event):
+        self.prev_page()
+
+    def next_page(self):
+        for panel in self.animal_list:
+            book = panel.m_simplebook
+            last_page = book.GetPageCount()
+            current_page = book.GetSelection()
+            if current_page < last_page:
+                book.ChangeSelection(current_page + 1)
+
+    def prev_page(self):
+        for panel in self.animal_list:
+            book = panel.m_simplebook
+            current_page = book.GetSelection()
+            if current_page > 0:
+                book.ChangeSelection(current_page - 1)
+
+    def onTrade(self, event):
+        self.model.trade()
+
+    def onReset(self, event):
+        self.model.reset()
+        self.Refresh()
+
+    def update_animals(self):
+        for animal in self.animal_list:
+            animal.set_control()
+        self.Refresh()
+
+    def onTradeRun(self, event):
+        self.m_timer.Start(500)
+
+    def onTradeStop(self, event):
+        self.m_timer.Stop()
+
+    def onTimer(self, event):
+        self.model.trade()
+
+    def onParameterSetting(self, event):
+        dialog = AnimalParameterSettingDialog(self)
+        result = dialog.ShowModal()
+        if result != wx.ID_OK:
+            return
+
+        for animal in self.animal_list:
+            animal.set_parameter(dialog)
 
 
 if __name__ == "__main__":
--- Market.py
+++ Market.py
@@ -1,16 +1,29 @@
-
+##################################################
+#
+#   取引処理
+#
+##################################################
 
 class Market:
     def __init__(self, animal_list, log):
-        self.animal_list = animal_list
-        self.log = log
+        """
+        取引処理クラス
+        :param animal_list: ModelのAnimalリスト
+        :param log: Viewのlog表示
+        """
+        self.animal_list = animal_list              # ModelのAnimalリスト
+        self.log = log                              # Viewのlog表示
 
     def trade(self):
+        """
+        取引をする
+        :return:
+        """
         for animal in self.animal_list:
             animal.production()  # 生産
 
         for animal_buyer in self.animal_list:
-            animal_buyer.buy(self.animal_list, self.log)
+            animal_buyer.buy(self.animal_list, self.log)    # 取引
 
         for animal in self.animal_list:
             animal.consume()  # 消費
