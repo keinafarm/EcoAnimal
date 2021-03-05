be464e74e5eece9137df3768778f00c86f824070
akira <akiracraftwork@infoseek.jp>
2021-03-05 11:01:25 +0900

    メインフレームにタイトル表示

--- SimpleBookSample.py
+++ SimpleBookSample.py
@@ -20,7 +20,7 @@ _ = gettext.gettext
 class MainFrame ( wx.Frame ):
 
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 1227,698 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = _(u"EcoAnimal"), pos = wx.DefaultPosition, size = wx.Size( 1227,698 ), style = wx.CAPTION|wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
